4f25600793ee705c96baadff382ae035ad84e8a0
Volodya <122814050+Vovchyck@users.noreply.github.com>
2023-02-20 18:13:41 +0100

    Task_11

---
+++ src/com/Homework_11/Truck.java
@@ -0,0 +1,11 @@
+package com.Homework_11;
+
+public class Truck extends Vehicle {
+   private boolean hasTrailer;
+   public Boolean getHasTrailer() {
+      return hasTrailer;
+   }
+   public void setHasTrailer(Boolean hasTrailer) {
+      this.hasTrailer = hasTrailer;
+   }
+}
---
+++ src/com/Homework_11/Vehicle.java
@@ -0,0 +1,22 @@
+package com.Homework_11;
+
+public class Vehicle {
+    private String model;
+    private String color;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
