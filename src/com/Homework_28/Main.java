9a1dbaee9714be4077562df8a7c26542989b5dcb
Volodya <122814050+Vovchyck@users.noreply.github.com>
2023-04-25 15:27:18 +0200

    Task_28

--- src/com/Homework_28/Main.java
+++ src/com/Homework_28/Main.java
@@ -6,26 +6,27 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        String path = "src/com/Homework_28/Text.txt";
-        Path path1 = Paths.get(path);
-        try {
-            System.out.println("Текст файла: ");
-            Files.readAllLines(path1).forEach(System.out::println);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        Scanner scanner = new Scanner(System.in);
-        try {
-            List<String> lines = Files.readAllLines(path1);
-            lines.add(scanner.nextLine());
-            Files.write(path1, lines);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } finally {
-            scanner.close();
-        }
-    }
-}
+ public class Main {
+     public static void main(String[] args) {
+         String path = "src/com/Homework_28/Text.txt";
+         Path path1 = Paths.get(path);
+         try {
+             System.out.println("Текст файла: ");
+             Files.readAllLines(path1).forEach(System.out::println);
+         } catch (IOException e) {
+             throw new RuntimeException(e);
+         }
+         Scanner scanner = new Scanner(System.in);
+         try {
+             List<String> lines = Files.readAllLines(path1);
+             System.out.println("Додайтестроку: ");
+             String textToAdd = scanner.nextLine();
+             lines.add(textToAdd);
+             Files.write(path1, lines);
+         } catch (IOException e) {
+             throw new RuntimeException(e);
+         } finally {
+             scanner.close();
+         }
+     }
+ }
